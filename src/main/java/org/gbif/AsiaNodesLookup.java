aa19524d8d52c5026f2deb2859561880829ebca0
Oliver Meyn <oliver@mineallmeyn.com>
2014-07-21 10:25:27 +0200

    minor tidying

--- src/main/java/org/gbif/AsiaNodesLookup.java
+++ src/main/java/org/gbif/AsiaNodesLookup.java
@@ -18,9 +18,11 @@ public class AsiaNodesLookup {
   private static final String[] GBIF_HEADERS =
     {"MatchType", "Confidence", "Synonym", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "ScientificName",
     "KingdomKey", "PhylumKey", "ClassKey", "OrderKey", "FamilyKey", "GenusKey", "NameUsageKey"};
+  private static final String HEADER_PREFIX = "GBIF_";
+  private static final byte[] NEWLINE = "\n".getBytes();
+  private static final byte[] COMMA = ",".getBytes();
 
   public static void main(String[] args) throws IOException {
-//    String inputFileName = "/Users/oliver/Documents/asian_nodes_mtg/ias_full.csv";
     String inputFileName = args[0];
     String outputFileName = args[1];
     File inputFile = new File(inputFileName);
@@ -28,17 +30,17 @@ public class AsiaNodesLookup {
     FileOutputStream os = new FileOutputStream(outputFile);
     CSVReader csvReader = new CSVReader(inputFile, "UTF-8", ",", '"', 0);
     String[] headerRow = csvReader.next();
-    byte[] comma = ",".getBytes();
+
     for (String field : headerRow) {
       os.write(field.getBytes());
-      os.write(comma);
+      os.write(COMMA);
     }
     for (String field : GBIF_HEADERS) {
-      String name = "GBIF_" + field;
+      String name = HEADER_PREFIX + field;
       os.write(name.getBytes());
-      os.write(comma);
+      os.write(COMMA);
     }
-    os.write("\n".getBytes());
+    os.write(NEWLINE);
 
     while (csvReader.hasNext()) {
       String[] fields = csvReader.next();
@@ -46,10 +48,9 @@ public class AsiaNodesLookup {
       // ID, K, P, C, O, F, G, sp epithet, sciName
       ParseResult<NameUsageMatch> result = NubLookupInterpreter
         .nubLookup(fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], null);
-      //      System.out.println(result.getPayload().toString());
       for (String field : fields) {
         os.write(field.getBytes());
-        os.write(comma);
+        os.write(COMMA);
       }
       NameUsageMatch match = result.getPayload();
       writeField(os, match.getMatchType().toString());
@@ -70,7 +71,7 @@ public class AsiaNodesLookup {
       writeField(os, match.getGenusKey());
       writeField(os, match.getUsageKey());
 
-      os.write("\n".getBytes());
+      os.write(NEWLINE);
     } os.close();
   }
 
@@ -82,16 +83,16 @@ public class AsiaNodesLookup {
   }
 
   private static void writeField(FileOutputStream os, String field) throws IOException {
-    String quoted = "\"" + field + "\"";
+    String quoted = '\"' + field + '\"';
     os.write(quoted.getBytes("UTF-8"));
-    os.write(",".getBytes("UTF-8"));
+    os.write(COMMA);
   }
 
   private static void logFields(String[] fields) {
     StringBuilder sb = new StringBuilder();
     for (String field : fields) {
-      sb.append(field + " ");
+      sb.append(field).append(' ');
     }
-    System.out.println(sb.toString());
+    LOG.debug(sb.toString());
   }
 }
